ba6b22c3aceb30b809bbe518e50853b48fa9d0e1
trungkay <trungkienng2410@gmail.com>
2022-04-03 19:44:55 +0700

    Login + Register Interface

---
+++ app/src/main/java/com/example/plink/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.plink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private LinearLayout linearLayout;
+    private ImageView btnShowPasswd;
+    private EditText txtPasswd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
