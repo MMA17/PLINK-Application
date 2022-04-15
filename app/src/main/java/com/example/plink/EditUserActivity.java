b51d453354e7779012a0d549679d831acfdedb8d
trungkay <trungkienng2410@gmail.com>
2022-04-15 21:34:04 +0700

    Them logic LOGIN + REGISER + DATE PICKER

--- app/src/main/java/com/example/plink/EditUserActivity.java
+++ app/src/main/java/com/example/plink/EditUserActivity.java
@@ -2,36 +2,70 @@ package com.example.plink;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
+import components.member.Member;
+
 public class EditUserActivity extends AppCompatActivity {
-    private Spinner spinnerPosition;
     private TextView txtPositon;
+    private Button btnUpdate,btnDOB;
+    private DatePickerDialog datePicker;
+    private Member member;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user);
-        txtPositon = findViewById(R.id.txtPosition);
-        spinnerPosition = findViewById(R.id.spinner_position);
-        String [] position = {"Giáo viên","Sinh viên"};
-        ArrayAdapter <String> adapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
-        this.spinnerPosition.setAdapter(adapter);
+        initDatePicker();
+        btnUpdate = findViewById(R.id.btnEdit);
+        btnDOB = findViewById(R.id.btnDOB);
+        btnDOB.setText(getDOB());
 
-        spinnerPosition.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Adapter a = adapterView.getAdapter();
-                String res = (String) a.getItem(i);
-                txtPositon.setText("Chức danh\n\t\t"+res);
-                Toast.makeText(getApplicationContext(),"Chức vụ:"+res,Toast.LENGTH_SHORT).show();
-            }
-        });
     }
+
+    private String getDOB() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month+=1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day,month,year);
+    }
+
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month +1;
+                String date = makeDateString(day,month,year);
+                btnDOB.setText(date);
+            }
+        };
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        datePicker =  new DatePickerDialog(this, AlertDialog.THEME_HOLO_LIGHT,dateSetListener,year,month,day);
+    }
+
+    private String makeDateString(int day, int month, int year) {
+        return day+"/"+month+"/"+year;
+    }
+
+    public void openDatePicker(View view){
+        datePicker.show();
+    }
+
 }
--- app/src/main/java/com/example/plink/ProfileActivity.java
+++ app/src/main/java/com/example/plink/ProfileActivity.java
@@ -2,13 +2,30 @@ package com.example.plink;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    private Button btnEdit;
+    private EditText name;
+    private static final int EDIT_CODE = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        btnEdit = findViewById(R.id.btnEdit);
+        name = findViewById(R.id.txtUsername);
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent t = new Intent();
+
+                startActivityForResult(t,EDIT_CODE);
+            }
+        });
     }
+
 }
