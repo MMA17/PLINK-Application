e76de2bd25861145726de1d5b766730ba28e667d
NMinhHoangg <hoanggngg2510@gmail.com>
2022-04-25 20:16:29 +0700

    commit

---
+++ app/src/main/java/com/example/plink/ClassMemberListActivity.java
@@ -0,0 +1,14 @@
+package com.example.plink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClassMemberListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_members_list);
+    }
+}
